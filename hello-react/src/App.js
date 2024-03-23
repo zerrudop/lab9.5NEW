476c86c6cda4f4be73c9d02031be0b6cf244b0e0
zerrudop <94140890+zerrudop@users.noreply.github.com>
2024-03-23 01:13:41 +0000

    resr

---
+++ hello-react/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="/images/linkedin.jpg" alt="logo" />
+        <p>
+          Buko Bop 4eva
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          I edited this text!
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
